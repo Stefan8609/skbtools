df2df7f8ed65b0a213dcd5f77c948f496f824e55
Stefan8609 <sk8609@princeton.edu>
2024-02-01 13:38:56 -0500

    Starting to add time alignment synthetic

---
+++ GeigerMethod/Bermuda_Data_Analyze.py
@@ -0,0 +1,63 @@
+"""
+Looks at the shape of the C-DOG files to get an understanding of what they look like
+"""
+
+
+import scipy.io as sio
+import numpy as np
+from simulatedAnnealing_Bermuda import simulatedAnnealing_Bermuda
+from GPS_Lever_Arms import GPS_Lever_arms
+import matplotlib.pyplot as plt
+
+#Load GNSS Data during the time of expedition (25 through 40.9) hours
+def load_and_process_data(path):
+    data = sio.loadmat(path)
+    days = data['days'].flatten() - 59015
+    times = data['times'].flatten()
+    datetimes = (days * 24 * 3600) + times
+    condition_GNSS = (datetimes/3600 >= 25) & (datetimes / 3600 <= 40.9)
+    time_GNSS = datetimes[condition_GNSS]/3600
+    x,y,z = data['x'].flatten()[condition_GNSS], data['y'].flatten()[condition_GNSS], data['z'].flatten()[condition_GNSS]
+    return time_GNSS, x,y,z
+
+paths = [
+    '../GPSData/Unit1-camp_bis.mat',
+    '../GPSData/Unit2-camp_bis.mat',
+    '../GPSData/Unit3-camp_bis.mat',
+    '../GPSData/Unit4-camp_bis.mat'
+]
+
+all_data = [load_and_process_data(path) for path in paths]
+common_datetimes = set(all_data[0][0])
+for data in all_data[1:]:
+    common_datetimes.intersection_update(data[0])
+common_datetimes = sorted(common_datetimes)
+
+filtered_data = []
+for datetimes, x, y, z in all_data:
+    mask = np.isin(datetimes, common_datetimes)
+    filtered_data.append([np.array(datetimes)[mask], np.array(x)[mask], np.array(y)[mask], np.array(z)[mask]])
+filtered_data = np.array(filtered_data)
+
+#Initialize Coordinates in form of Geiger's Method
+GPS_Coordinates = np.zeros((len(filtered_data[0,0]),4,3))
+for i in range(len(filtered_data[0,0])):
+    for j in range(4):
+        GPS_Coordinates[i, j, 0] = filtered_data[j, 1, i]
+        GPS_Coordinates[i, j, 1] = filtered_data[j, 2, i]
+        GPS_Coordinates[i, j, 2] = filtered_data[j, 3, i]
+
+#Initialize Dog Acoustic Data
+
+#offset:RMSE, 68116:222.186, 68126:165.453, 68136:219.04, 68130:184.884, 68128: 170.04, 68124: 168.05, 68125:167
+offset = 68126#66828#68126 This is approximately overlaying them now
+data_DOG = sio.loadmat('../GPSData/DOG1-camp.mat')['tags'].astype(float)
+acoustic_DOG = np.unwrap(data_DOG[:,1] / 1e9*2*np.pi) / (2*np.pi)  #Numpy page describes how unwrap works
+    #I don't think the periodicity for unwrap function is 2*pi as what is set now
+time_DOG = (data_DOG[:, 0] + offset) / 3600
+# condition_DOG = (time_DOG >=25) & (time_DOG <= 40.9)
+# time_DOG, acoustic_DOG = time_DOG[condition_DOG], acoustic_DOG[condition_DOG]
+
+plt.scatter(list(range(len(acoustic_DOG))),acoustic_DOG + data_DOG[:,0], s=1)
+# plt.scatter(list(range(len(acoustic_DOG))),acoustic_DOG, s=1)
+plt.show()
---
+++ GeigerMethod/Synthetic/Time_Alignment.py
@@ -0,0 +1,123 @@
+"""
+Python file to investigate the affects of the time alignment file
+Trying to mimic the CDOG file that we have, and the GPS data that we have
+
+GPS will be offset from CDOG by travel time
+Data sets will be offset by some random amount of time
+Swaths of CDOG data points are removed at random
+
+Could use GPS data from our experiment at some point
+"""
+
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from RigidBodyMovementProblem import findRotationAndDisplacement
+
+esv_table = sio.loadmat('../../GPSData/global_table_esv.mat')
+dz_array = esv_table['distance'].flatten()
+angle_array = esv_table['angle'].flatten()
+esv_matrix = esv_table['matrice']
+
+def find_esv(beta, dz):
+    idx_closest_dz = np.searchsorted(dz_array, dz, side="left")
+    idx_closest_dz = np.clip(idx_closest_dz, 0, len(dz_array)-1)
+    idx_closest_beta = np.searchsorted(angle_array, beta, side="left")
+    idx_closest_beta = np.clip(idx_closest_beta, 0, len(angle_array)-1)
+    closest_esv = esv_matrix[idx_closest_dz, idx_closest_beta]
+    return closest_esv
+
+def calculateTimesRayTracing(guess, transponder_coordinates):
+    hori_dist = np.sqrt((transponder_coordinates[:, 0] - guess[0])**2 + (transponder_coordinates[:, 1] - guess[1])**2)
+    abs_dist = np.linalg.norm(transponder_coordinates - guess, axis=1)
+    beta = np.arccos(hori_dist / abs_dist) * 180 / np.pi
+    dz = np.abs(guess[2] - transponder_coordinates[:, 2])
+    esv = find_esv(beta, dz)
+    times = abs_dist / esv
+    return times, esv
+
+def findTransponder(GPS_Coordinates, gps1_to_others, gps1_to_transponder):
+    xs, ys, zs = gps1_to_others.T
+    initial_transponder = gps1_to_transponder
+    n = len(GPS_Coordinates)
+    transponder_coordinates = np.zeros((n, 3))
+    for i in range(n):
+        new_xs, new_ys, new_zs = GPS_Coordinates[i].T
+        R_mtrx, d = findRotationAndDisplacement(np.array([xs,ys,zs]), np.array([new_xs, new_ys, new_zs]))
+        transponder_coordinates[i] = np.matmul(R_mtrx, initial_transponder) + d
+    return transponder_coordinates
+
+#Initialize Bermuda GPS
+def load_and_process_data(path):
+    data = sio.loadmat(path)
+    days = data['days'].flatten() - 59015
+    times = data['times'].flatten()
+    datetimes = (days * 24 * 3600) + times
+    condition_GNSS = (datetimes/3600 >= 25) & (datetimes / 3600 <= 40.9)
+    time_GNSS = datetimes[condition_GNSS]/3600
+    x,y,z = data['x'].flatten()[condition_GNSS], data['y'].flatten()[condition_GNSS], data['z'].flatten()[condition_GNSS]
+    return time_GNSS, x,y,z
+
+paths = [
+    '../../GPSData/Unit1-camp_bis.mat',
+    '../../GPSData/Unit2-camp_bis.mat',
+    '../../GPSData/Unit3-camp_bis.mat',
+    '../../GPSData/Unit4-camp_bis.mat'
+]
+
+all_data = [load_and_process_data(path) for path in paths]
+common_datetimes = set(all_data[0][0])
+for data in all_data[1:]:
+    common_datetimes.intersection_update(data[0])
+common_datetimes = sorted(common_datetimes)
+
+filtered_data = []
+for datetimes, x, y, z in all_data:
+    mask = np.isin(datetimes, common_datetimes)
+    filtered_data.append([np.array(datetimes)[mask], np.array(x)[mask], np.array(y)[mask], np.array(z)[mask]])
+filtered_data = np.array(filtered_data)
+
+#Initialize Coordinates in form of Geiger's Method
+GPS_Coordinates = np.zeros((len(filtered_data[0,0]),4,3))
+for i in range(len(filtered_data[0,0])):
+    for j in range(4):
+        GPS_Coordinates[i, j, 0] = filtered_data[j, 1, i]
+        GPS_Coordinates[i, j, 1] = filtered_data[j, 2, i]
+        GPS_Coordinates[i, j, 2] = filtered_data[j, 3, i]
+
+GPS_Coordinates = GPS_Coordinates - np.mean(GPS_Coordinates, axis=0)
+gps1_to_others = np.array([[0,0,0],[-2.4054, -4.20905, 0.060621], [-12.1105,-0.956145,0.00877],[-8.70446831,5.165195, 0.04880436]])
+initial_lever_guess = np.array([-12.4, 15.46, -15.24])
+transponder_coordinates = findTransponder(GPS_Coordinates, gps1_to_others, initial_lever_guess)
+CDOG = [-1000, -1000, -5000]
+
+CDOG_time = calculateTimesRayTracing(CDOG, transponder_coordinates)[0]
+CDOG_time += np.arange(len(CDOG_time))
+CDOG_remain, CDOG_int = np.modf(CDOG_time)
+
+"""Remove values at random indices"""
+# removal_indices = np.random.choice(len(CDOG_time), 10000, replace=False)
+# CDOG_remain = np.delete(CDOG_remain, removal_indices)
+# CDOG_time = np.delete(CDOG_time, removal_indices)
+
+
+print(CDOG_remain[:200])
+acoustic_DOG = np.unwrap(CDOG_remain * 2 * np.pi) / (2*np.pi)  #Numpy page describes how unwrap works
+
+test = acoustic_DOG + CDOG_time[0]
+print(test)
+
+plt.scatter(list(range(len(acoustic_DOG))), test, s=1)
+
+
+# plt.scatter(list(range(len(CDOG_time))), CDOG_time, s=1)
+# plt.scatter(GPS_Coordinates[:,0,0],GPS_Coordinates[:,0,1], s=1)
+# plt.scatter(CDOG[0], CDOG[1])
+plt.show()
+
+"""
+Find a way to save CDOG_time to a matlab file in the same way that the CDOG data is actually saved
+Try to get the data to mimic what we see in the real data
+
+Currently x,y,z of dog is not very good (the ecef coords don't have z correspondign to depth)
+"""
