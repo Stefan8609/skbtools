bfe51cbb7d5152c2b815e1f93bd9f96f32ef1648
Stefan8609 <sk8609@princeton.edu>
2024-02-21 15:31:43 -0500

    New Additions

--- GeigerMethod/Synthetic/Align_Geiger_Annealing.py
+++ GeigerMethod/Synthetic/Align_Geiger_Annealing.py
@@ -1,5 +1,9 @@
 import numpy as np
-from Unwrap_Fix import RMSE_offset, find_offset
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from advancedGeigerMethod import calculateTimesRayTracing
+
+from Unwrap_Fix import align
 
 """
 Goal of this file is to iterate through finding the CDOG with the following steps:
@@ -8,5 +12,22 @@ Goal of this file is to iterate through finding the CDOG with the following step
     3) Use Synthetic Annealing to find the best guess for the transducer location (with a constricted range of possibilities)
     4) Iterate back to the alignment phase with the new CDOG guess and travel time series
     5) After a certain number of iterations take the given CDOG location (determine validity by looking at residual distribution)
+    
+Gotta rename indices data_DOG and dog_data are not discernible names lol
 """
+
+data_DOG = sio.loadmat('../../GPSData/Synthetic_CDOG_noise.mat')['tags'].astype(float)
+acoustic_DOG = np.unwrap(data_DOG[:, 1] * 2 * np.pi) / (2 * np.pi)
+
+transponder_coordinates = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['xyz'].astype(float)
+GPS_time = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['time'].astype(float)
+
+CDOG = np.array([-1979.9094551, 4490.73551826, -2011.85148619])
+travel_times = calculateTimesRayTracing(CDOG, transponder_coordinates)[0]
+
+# TEST (I like it!) -> This starts the unwrapping at a reasonable value instead of 0 time
+acoustic_DOG += travel_times[0] - acoustic_DOG[0]
+
+full_times, dog_data, GPS_data = align(data_DOG, acoustic_DOG, GPS_time, travel_times)
+
 
--- GeigerMethod/Synthetic/Unwrap_Fix.py
+++ GeigerMethod/Synthetic/Unwrap_Fix.py
@@ -9,42 +9,9 @@ Should make this alignment process more efficient (right now it too slow)
     Possibly could do this using cross correlation ? Cuz this can use FFT
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io as sio
-from advancedGeigerMethod import calculateTimesRayTracing
-
-data_DOG = sio.loadmat('../../GPSData/Synthetic_CDOG_noise.mat')['tags'].astype(float)
-acoustic_DOG = np.unwrap(data_DOG[:,1] *2*np.pi) / (2*np.pi)
-
-transponder_coordinates = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['xyz'].astype(float)
-GPS_time = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['time'].astype(float)
-
-CDOG = np.array([-1979.9094551, 4490.73551826, -2011.85148619])
-travel_times = calculateTimesRayTracing(CDOG, transponder_coordinates)[0]
-
-#TEST (I like it!)
-acoustic_DOG += travel_times[0] - acoustic_DOG[0]
-
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 10))
-
-ax1.scatter(data_DOG[:,0] + data_DOG[:,1], acoustic_DOG, s=1, color='r', label="Synthetic Dog Travel Time")
-ax1.legend(loc="upper right")
-ax1.set_xlabel("Arrivals in Absolute Time (s)")
-ax1.set_ylabel("Unwrapped Travel Time (s)")
-ax1.set_title("Synthetic travel times")
-
-diff_plot_data = np.diff(acoustic_DOG)
-ax2.scatter(data_DOG[:len(data_DOG)-1,0] + data_DOG[:len(data_DOG)-1,1], diff_plot_data, s=1)
-ax2.set_xlabel("Index")
-ax2.set_ylabel("Difference between unwrapped times (s)")
-ax2.set_title("Relative Change in DOG data")
-
-plt.show()
-
 #Make this capable of handling vectorization
 #Make functions more flexible with parameters (basically just parameterize all variables that are inputs)
-def RMSE_offset(offset):
+def RMSE_offset(offset, data_DOG, acoustic_DOG, GPS_time, travel_times):
     # Get DOG indexed approximately with time
     times_DOG = np.round(data_DOG[:, 0] + data_DOG[:, 1])
     times_GPS = np.round(GPS_time[0] + travel_times + offset)
@@ -53,7 +20,7 @@ def RMSE_offset(offset):
     unique_times_DOG, indices_DOG = np.unique(times_DOG, return_index=True)
     unique_acoustic_DOG = acoustic_DOG[indices_DOG]
 
-    # Get unique times and corresponding travel times for GPS
+    # Get unique times and corresponding travel times for GPS (perhaps get rid of both repeated points)
     unique_times_GPS, indices_GPS = np.unique(times_GPS, return_index=True)
     unique_travel_times = travel_times[indices_GPS]
 
@@ -65,8 +32,18 @@ def RMSE_offset(offset):
     GPS_data = np.full(full_times.shape, np.nan)
 
     # Assign values to DOG and GPS data arrays
-    dog_data[np.isin(full_times, unique_times_DOG)] = unique_acoustic_DOG
-    GPS_data[np.isin(full_times, unique_times_GPS)] = unique_travel_times
+    # dog_data[np.isin(full_times, unique_times_DOG)] = unique_acoustic_DOG
+    # GPS_data[np.isin(full_times, unique_times_GPS)] = unique_travel_times
+
+    # For dog_data
+    indices_dog = np.searchsorted(full_times, unique_times_DOG)
+    mask_dog = (indices_dog < len(full_times)) & (full_times[indices_dog] == unique_times_DOG)
+    dog_data[indices_dog[mask_dog]] = unique_acoustic_DOG[mask_dog]
+
+    # For GPS_data
+    indices_gps = np.searchsorted(full_times, unique_times_GPS)
+    mask_gps = (indices_gps < len(full_times)) & (full_times[indices_gps] == unique_times_GPS)
+    GPS_data[indices_gps[mask_gps]] = unique_travel_times[mask_gps]
 
     #Remove nan values from the three arrays
     mask = ~np.isnan(dog_data) & ~np.isnan(GPS_data)
@@ -76,59 +53,101 @@ def RMSE_offset(offset):
 
     # Calculate RMSE
     RMSE = np.sqrt(np.nanmean((np.modf(GPS_data)[0] - np.modf(dog_data)[0]) ** 2))
+
     return RMSE, full_times, dog_data, GPS_data
 
 #Only use this up to integer differences
-def find_offset():
+def find_offset(data_DOG, acoustic_DOG, GPS_time, travel_times):
     l, u = 0, 10000
     intervals = np.array([100, 10, 1])
     best_offset = 0
     for interval in intervals:
         offsets = np.arange(l, u, interval)
-        vals = np.array([RMSE_offset(offset)[0] for offset in offsets])
+        vals = np.array([RMSE_offset(offset, data_DOG, acoustic_DOG, GPS_time, travel_times)[0] for offset in offsets])
         best_index = np.argmin(vals)
         best_offset = offsets[best_index]
         l, u = best_offset - interval, best_offset + interval
     return best_offset
 
-#Steps to alignment: Find offset, adjust for integer differences
-def align():
+#Steps to alignment: Find offset, adjust for integer differences, return time series
+def align(data_DOG, acoustic_DOG, GPS_time, travel_times):
+    #Here we have "acoustic_DOG += travel_times[0] - acoustic_DOG[0]" Later
 
-    return
+    #Find offset, and get new time series corresponding with that offset
+    offset = find_offset(data_DOG, acoustic_DOG, GPS_time, travel_times)
+    full_times, dog_data, GPS_data = RMSE_offset(offset, data_DOG, acoustic_DOG, GPS_time, travel_times)[1:]
 
-
-offset = find_offset()
-
-#Use offset found from find_offset in the future
-full_times, dog_data, GPS_data = RMSE_offset(offset)[1:]
-
-for i in range(len(full_times)):
     #If off by around an integer, adjust by that integer amount
-    if np.abs(dog_data[i] - GPS_data[i]) >= .9:
-        dog_data[i] += np.round(GPS_data[i] - dog_data[i])
+    abs_diff = np.abs(dog_data - GPS_data)
+    indices = np.where(abs_diff >= 0.9)
+    dog_data[indices] += np.round(GPS_data[indices] - dog_data[indices])
 
-print(np.sqrt(np.nanmean((dog_data - GPS_data)**2))*1515*100, "cm")
+    #Plot overlaying time series and the residual plot
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9))
 
-diff_data = dog_data - GPS_data
+    ax1.scatter(full_times, dog_data, s=10, marker="x", label="Unwrapped/Adjusted Synthetic Dog Travel Time")
+    ax1.scatter(full_times, GPS_data, s=1, marker="o", label="Calculated GPS Travel Times")
+    ax1.legend(loc="upper right")
+    ax1.set_xlabel("Arrivals in Absolute Time (s)")
+    ax1.set_ylabel("Travel Times (s)")
+    ax1.set_title("Synthetic travel times")
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 9))
+    diff_data = dog_data - GPS_data
+    ax2.scatter(full_times, diff_data, s=1)
+    ax2.set_xlabel("Absolute Time (s)")
+    ax2.set_ylabel("Difference between calculated and unwrapped times (s)")
+    ax2.set_title("Residual Plot")
 
-ax1.scatter(full_times, dog_data, s=10, marker="x", label="Unwrapped/Adjusted Synthetic Dog Travel Time")
-ax1.scatter(full_times, GPS_data, s=1, marker="o", label="Calculated GPS Travel Times")
-ax1.legend(loc="upper right")
-ax1.set_xlabel("Arrivals in Absolute Time (s)")
-ax1.set_ylabel("Travel Times (s)")
-ax1.set_title("Synthetic travel times")
+    plt.show()
+    return full_times, dog_data, GPS_data
 
-ax2.scatter(full_times, diff_data, s=1)
-ax2.set_xlabel("Absolute Time (s)")
-ax2.set_ylabel("Difference between calculated and unwrapped times (s)")
-ax2.set_title("Residual Plot")
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import scipy.io as sio
+    from advancedGeigerMethod import calculateTimesRayTracing
 
-plt.show()
+    data_DOG = sio.loadmat('../../GPSData/Synthetic_CDOG_noise.mat')['tags'].astype(float)
+    acoustic_DOG = np.unwrap(data_DOG[:, 1] * 2 * np.pi) / (2 * np.pi)
+
+    transponder_coordinates = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['xyz'].astype(float)
+    GPS_time = sio.loadmat('../../GPSData/Synthetic_transponder_noise.mat')['time'].astype(float)
+
+    CDOG = np.array([-1979.9094551, 4490.73551826, -2011.85148619])
+    travel_times = calculateTimesRayTracing(CDOG, transponder_coordinates)[0]
+
+    # TEST (I like it!)
+    acoustic_DOG += travel_times[0] - acoustic_DOG[0]
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 10))
+
+    ax1.scatter(data_DOG[:, 0] + data_DOG[:, 1], acoustic_DOG, s=1, color='r', label="Synthetic Dog Travel Time")
+    ax1.legend(loc="upper right")
+    ax1.set_xlabel("Arrivals in Absolute Time (s)")
+    ax1.set_ylabel("Unwrapped Travel Time (s)")
+    ax1.set_title("Synthetic travel times")
+
+    diff_plot_data = np.diff(acoustic_DOG)
+    ax2.scatter(data_DOG[:len(data_DOG) - 1, 0] + data_DOG[:len(data_DOG) - 1, 1], diff_plot_data, s=1)
+    ax2.set_xlabel("Index")
+    ax2.set_ylabel("Difference between unwrapped times (s)")
+    ax2.set_title("Relative Change in DOG data")
+
+    plt.show()
+
+    plt.scatter(data_DOG[:, 0] + data_DOG[:, 1], acoustic_DOG, s=1, color='r', label="Synthetic Dog Travel Time")
+    plt.scatter(GPS_time, travel_times, s=1, color='b', label="Calculated GPS travel times")
+    plt.xlabel("Absolute Time (s)")
+    plt.ylabel("Difference between calculated and unwrapped times (s)")
+    plt.title("Residual Plot")
+    plt.show()
+
+    full_times, dog_data, GPS_data = align(data_DOG, acoustic_DOG, GPS_time, travel_times)
+
+    print(np.sqrt(np.nanmean((dog_data - GPS_data) ** 2)) * 1515 * 100, "cm")
 
 
-sio.savemat("../../GPSData/Aligned_Synthetic.mat", {"full_times": full_times, "dog_data": dog_data, "GPS_data": GPS_data})
+# sio.savemat("../../GPSData/Aligned_Synthetic.mat", {"full_times": full_times, "dog_data": dog_data, "GPS_data": GPS_data})
 
 
 """comments below"""
