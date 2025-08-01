a1cf94eb2183eeda603e9ca71ab1f534bab76282
Stefan8609 <sk8609@princeton.edu>
2025-07-31 21:56:32 -0400

    mcmc prior

--- src/GeigerMethod/Synthetic/Numba_Functions/MCMC_sampler.py
+++ src/GeigerMethod/Synthetic/Numba_Functions/MCMC_sampler.py
@@ -392,7 +392,7 @@ if __name__ == "__main__":
     offsets = np.array([1866.0, 3175.0, 1939.0])
 
     # initial parameters
-    init_lever = np.array([-12.4659, 9.6021, -16.2993])
+    init_lever = np.array([-12.8, 9.2, -15.9])
     init_gps_grid = np.array(
         [
             [0.0, 0.0, 0.0],
@@ -411,9 +411,23 @@ if __name__ == "__main__":
     # init_ebias = np.array([-0.4775, -0.3199, 0.1122])
     values = np.array([-0.4775, -0.3199, 0.1122])
     # values = np.array([0.4775, 0.3199, -0.1122])
-    n = 3  # number of splits for ESV bias
+    n = 7  # number of splits for ESV bias
     init_ebias = np.tile(values.reshape(-1, 1), (1, n))
     init_tbias = np.array([0.01518602, 0.015779, 0.018898])
+
+    # proposal scales
+    proposal_lever = np.array([0.01, 0.01, 0.05])
+    proposal_gps_grid = 0.0
+    proposal_CDOG_aug = 0.1
+    proposal_esv_bias = 0.01
+    proposal_time_bias = 0.000005
+
+    # prior scales
+    prior_lever = np.array([0.3, 0.3, 0.3])
+    prior_gps_grid = 0.1
+    prior_CDOG_aug = 3.0
+    prior_esv_bias = 1.0
+    prior_time_bias = 0.5
 
     (
         lever_chain,
@@ -438,6 +452,16 @@ if __name__ == "__main__":
         CDOG_reference=CDOG_reference,
         CDOG_all_data=typed_CDOG_all_data,
         offsets=offsets,
+        proposal_lever=proposal_lever,
+        proposal_gps_grid=proposal_gps_grid,
+        proposal_CDOG_aug=proposal_CDOG_aug,
+        proposal_esv_bias=proposal_esv_bias,
+        proposal_time_bias=proposal_time_bias,
+        prior_lever=prior_lever,
+        prior_gps_grid=prior_gps_grid,
+        prior_CDOG_aug=prior_CDOG_aug,
+        prior_esv_bias=prior_esv_bias,
+        prior_time_bias=prior_time_bias,
     )
 
     # now you can repackage as a dict in normal Python:
